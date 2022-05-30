5c76dd940ae771442170ea228392a25908250d20
emilyliu1223 <liu.tingyu@northeastern.edu>
2022-05-30 14:08:17 -0400

    comit

--- keeper/src/index.js
+++ keeper/src/index.js
@@ -3,7 +3,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import { BrowserRouter as Router } from "react-router-dom";
-ReactDOM.render(<Router basename={/github.io/.test(window.location) ? "/Web-Projects/keeper" : ""}>
-<App />
-</Router>, document.getElementById("root"));
+
+ReactDOM.render(<App />, document.getElementById("root"));
